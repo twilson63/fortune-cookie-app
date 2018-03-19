28923533fd39acccccb560d0265a75ad0645b2e0
Tom Wilson <tom@jackhq.com>
2018-03-19 16:41:55 -0400

    added apploading component

--- app/App.js
+++ app/App.js
@@ -4,6 +4,8 @@ import fortuneCookies from 'fortune-cookie'
 import randomInt from 'random-int'
 import Container from 'react-declarative-container'
 import FortuneCookieImage from './fortune-cookie.png'
+import { Asset, AppLoading } from 'expo'
+import { cond, equals, always } from 'ramda'
 
 const action = (type, payload) => ({ type, payload })
 
@@ -12,41 +14,64 @@ export default class App extends React.Component {
     return (
       <Container
         initialState={{
+          status: 'LOADING',
           fortune: 'press the button and I will predict your future...'
         }}
       >
-        {({ fortune, dispatch }) => (
-          <View style={styles.container}>
-            <Image
-              style={{ height: 100, width: 100 }}
-              source={FortuneCookieImage}
-            />
-            <Text style={{ fontSize: 24, color: 'rgba(0,0,0,.28)' }}>
-              Fortune Cookie
-            </Text>
-            <Text style={{ marginHorizontal: 16, marginTop: 8 }}>
-              {fortune}
-            </Text>
-            <TouchableHighlight
-              style={{
-                margin: 16,
-                borderWidth: 2,
-                padding: 8,
-                borderRadius: 4
-              }}
-              onPress={() =>
-                dispatch(
-                  action(
-                    'fortune',
-                    fortuneCookies[randomInt(fortuneCookies.length - 1)]
-                  )
-                )
-              }
-            >
-              <Text>Predict My Future</Text>
-            </TouchableHighlight>
-          </View>
-        )}
+        {({ fortune, status, dispatch }) =>
+          cond([
+            [
+              equals('LOADING'),
+              always(
+                <AppLoading
+                  startAsync={() => {
+                    // TODO: Cache Images and Fonts...
+                    Promise.resolve(true)
+                  }}
+                  onError={console.warn}
+                  onFinish={() => {
+                    dispatch(action('status', 'READY'))
+                  }}
+                />
+              )
+            ],
+            [
+              equals('READY'),
+              always(
+                <View style={styles.container}>
+                  <Image
+                    style={{ height: 100, width: 100 }}
+                    source={FortuneCookieImage}
+                  />
+                  <Text style={{ fontSize: 24, color: 'rgba(0,0,0,.28)' }}>
+                    Fortune Cookie
+                  </Text>
+                  <Text style={{ marginHorizontal: 16, marginTop: 8 }}>
+                    {fortune}
+                  </Text>
+                  <TouchableHighlight
+                    style={{
+                      margin: 16,
+                      borderWidth: 2,
+                      padding: 8,
+                      borderRadius: 4
+                    }}
+                    onPress={() =>
+                      dispatch(
+                        action(
+                          'fortune',
+                          fortuneCookies[randomInt(fortuneCookies.length - 1)]
+                        )
+                      )
+                    }
+                  >
+                    <Text>Predict My Future</Text>
+                  </TouchableHighlight>
+                </View>
+              )
+            ]
+          ])(status)
+        }
       </Container>
     )
   }
